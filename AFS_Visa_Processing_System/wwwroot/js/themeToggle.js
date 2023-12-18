69a273cdcaa6f67c36baba59f0106127dd8949cf
Kieran-git <64077345+Kieran-git@users.noreply.github.com>
2023-12-18 17:36:35 +0000

    started dark theme toggle

--- AFS_Visa_Processing_System/wwwroot/js/themeToggle.js
+++ AFS_Visa_Processing_System/wwwroot/js/themeToggle.js
@@ -1,10 +1,6 @@
-$(document).ready(function () {
-    $("#btnThemeSwtich").click(function () {
-        if (document.body.classList.contains('dark')) {
-            document.body.classList.remove('dark');
-        } else {
-            document.body.classList.add('dark');
-        }
-        $("#darkTheme").innerText = document.body.classList;
-    });
-});
+function themeToggle() {
+    
+    console.log("test");
+    $("body").toggleClass("bg-secondary");
+    $("nav").toggleClass("navbar-dark bg-dark");
+}
