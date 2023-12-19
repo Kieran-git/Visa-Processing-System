e58eb59e46de16ba22a6173b233febb1ed121320
Kieran-git <64077345+Kieran-git@users.noreply.github.com>
2023-12-19 11:55:43 +0000

    Dark and light theme toggle done

--- AFS_Visa_Processing_System/wwwroot/js/themeToggle.js
+++ AFS_Visa_Processing_System/wwwroot/js/themeToggle.js
@@ -1,6 +1,18 @@
 function themeToggle() {
     
-    console.log("test");
-    $("body").toggleClass("bg-secondary");
+    // Add
     $("nav").toggleClass("navbar-dark bg-dark");
+    
+    $("body").toggleClass("bg-secondary text-white");
+
+    $("li.list-group-item").toggleClass("bg-secondary text-white")
+
+    $("#btnThemeToggle").toggleClass("btn-dark");
+
+    $("div.step-container").toggleClass("text-black");
+    $("div.form-text").toggleClass("text-white");
+
+    // Remove
+    $("nav").toggleClass("border-bottom border-2"); 
+    
 }
