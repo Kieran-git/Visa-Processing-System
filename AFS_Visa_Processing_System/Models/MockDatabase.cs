b0d28618718541ba7f990da17471854970d7fa55
Kieran-git <64077345+Kieran-git@users.noreply.github.com>
2023-12-22 15:54:40 +0000

    Prep for architectural validation demonstration video

--- AFS_Visa_Processing_System/Models/MockDatabase.cs
+++ AFS_Visa_Processing_System/Models/MockDatabase.cs
@@ -1,16 +1,17 @@
 ﻿using AFS_Visa_Processing_System.Pages;
 using Newtonsoft.Json;
+using System.Net.Sockets;
 
 namespace AFS_Visa_Processing_System.Models
 {
     public static class MockDatabase
     {
-        private static readonly string dataDirectory = @"C:\Users\Kieran\source\repos\SAD-Assessment-Visa-Processing-System\AFS_Visa_Processing_System\Data\";
         public static bool WriteToVisaApplication(VisaApplication visaApplication)
         {
             try
             {
-                File.AppendAllText(Path.Combine(dataDirectory, "VisaApplicationTable.txt"), JsonConvert.SerializeObject(visaApplication) + Environment.NewLine);
+                // This will only work on the server side of the blazor app so for this client only app it is mocked
+                // File.AppendAllText(Path.Combine(dataDirectory, "VisaApplicationTable.txt"), JsonConvert.SerializeObject(visaApplication) + Environment.NewLine);
             }
             catch
             {
@@ -27,7 +28,8 @@ namespace AFS_Visa_Processing_System.Models
         {
             try
             {
-                File.AppendAllText(Path.Combine(dataDirectory, "Logs.txt"), JsonConvert.SerializeObject(@event) + Environment.NewLine);
+                // This will only work on the server side of the blazor app so for this client only app it is mocked
+                // File.AppendAllText(Path.Combine(dataDirectory, "Logs.txt"), JsonConvert.SerializeObject(@event) + Environment.NewLine);
             }
             catch
             {
